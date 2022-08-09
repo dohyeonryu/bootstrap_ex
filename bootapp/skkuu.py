7e1ff071b360a3de9603e7864f8bd8098ca3c5d8
dohyeonryu <91361206+dohyeonryu@users.noreply.github.com>
2022-08-09 10:23:51 +0900

    Update skkuu.py

--- bootapp/skkuu.py
+++ bootapp/skkuu.py
@@ -57,8 +57,9 @@ class SKKU_SENTIMENT:
 			for input_ids_batch, attention_masks_batch, y_batch in tqdm(train_loader):
 				
 				optimizer.zero_grad()
-				y_batch = y_batch.type(torch.LongTensor)
-				y_batch = y_batch.to(self.device)
+				no_deprecation_warning=True
+				#y_batch = y_batch.type(torch.LongTensor)
+				#y_batch = y_batch.to(self.device)
 				y_pred = self.model(input_ids_batch.to(self.device), attention_mask=attention_masks_batch.to(self.device))[0]
 				loss = F.cross_entropy(y_pred, y_batch)
 				loss.backward()
